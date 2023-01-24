e18f485a90958254c256039ab5eaf4ec3ec22546
BrantleyH <123440280+BrantleyH@users.noreply.github.com>
2023-01-23 23:48:33 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 2.5;
+        double y = 3.3;
+        double z;
+        z = 4*x*x+3*y;
+        Console.WriteLine("X= "+x);
+        Console.WriteLine("\tY= "+y);
+        Console.WriteLine("The Value of Z is: "+z);
+        
+    }
+}
