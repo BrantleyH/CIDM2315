0b87d90ba99adcfbea70d666c9804221689cc867
BrantleyH <123440280+BrantleyH@users.noreply.github.com>
2023-02-06 22:13:24 -0600

    Add files via upload

---
+++ Homework3Q2.cs
@@ -0,0 +1,28 @@
+﻿namespace Homework3Q2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Brantley Hudson");
+
+        using System;
+        class Pattern
+        {
+            Console.WriteLine("assign an int value to N: ");
+                int n = int.Parse(Console.ReadLine());
+
+                for(int i = 0;
+                    int<n;
+                    int++)
+                {
+                    for(int j=0;
+                            j<n;
+                            j++)
+                {
+                    Console.Write("#");
+                }
+                    Console.Write();
+                }
+        }
+    }
+}
---
+++ Homework3Q3.cs
@@ -0,0 +1,36 @@
+﻿namespace Homework3Q3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Brantley Hudson");
+        
+        using System;
+        public class Program
+        {
+            public static void Main()
+        {
+            Console.WriteLine("Assign an int value to N: ");
+
+            int N=Convert.ToInt16(Console.ReadLine());
+
+            for(int i=0;
+                    i<N;
+                    i++;)
+            {
+                for(int j=0;
+                        j<N;
+                        j++)
+            {
+                if(int<j)
+                    Console.WriteLine(" ");
+                
+                else
+                    Console.WriteLine("*");
+            }
+            Console.Write("\n");
+            }
+        }
+        }
+    }
+}
