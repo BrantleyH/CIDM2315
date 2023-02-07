c134d56bc77fc77f76ceba3d1d03de8c553b6da7
BrantleyH <123440280+BrantleyH@users.noreply.github.com>
2023-02-06 21:40:43 -0600

    Add files via upload

---
+++ Homework3.cs
@@ -0,0 +1,38 @@
+﻿namespace Homework3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Brantley Hudson");
+
+        bool flag = true;
+        Console.WriteLine("Input an integer: ");
+
+        string input =
+            Console.ReadLine();
+        
+        int number = 
+            Convert.ToInt32(intput);
+                
+        for(int i=2;
+                i<number/2;
+                i++)
+        {
+            if(number%i==0)
+            {
+                flag = false;
+                break;
+            }
+        }
+
+        if(flag)
+        {
+            Console.WriteLine(input +"is Prime ");
+        }
+
+        else{
+            Console.WriteLine(input+" is non-prime ");
+        }
+
+    }
+}
