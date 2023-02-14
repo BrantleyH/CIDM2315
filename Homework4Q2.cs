c4fa0d355abec1d91859064aa3151eef1090cf66
BrantleyH <123440280+BrantleyH@users.noreply.github.com>
2023-02-13 23:01:49 -0600

    Add files via upload

---
+++ Homework4Q2.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework4Q2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Brantley Hudson");
+        Console.WriteLine("CDIM2315 Homework4Q2");
+
+        int i, j, rows;
+         printf ("Enter the number of rows: ");
+         scanf ("%d", &rows);
+         for (i = 1; i <= rows; ++i)
+         {
+            for (j = 1; j <= i; ++j)
+            {
+                printf("\n");
+            }
+            return 0;
+         }
+    }
+}
