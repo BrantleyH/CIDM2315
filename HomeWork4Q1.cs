83710b81919cad1763fcfdfae0e604d0d50d158c
BrantleyH <123440280+BrantleyH@users.noreply.github.com>
2023-02-13 23:56:08 -0600

    Add files via upload

---
+++ HomeWork4Q1.cs
@@ -0,0 +1,34 @@
+﻿namespace Homework4;
+class Program
+{
+    static int largest(int a, int b)
+    {
+        if(a > b)
+        {
+            return a;
+        }
+
+        else if(b > a)
+        {
+            return b;
+        }
+
+        else
+        {
+            return -1;
+        }
+    }
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Brantley Hudson");
+        
+        int a = 3, b = 5;
+        Console.WriteLine($"a= {a},b={b}");
+
+        int lar= largest(a, b);
+
+        Console.WriteLine("\nLargest Number is: "+lar);
+
+    }
+}
---
+++ HomeWork4Q2.cs
@@ -0,0 +1,53 @@
+﻿namespace Homework4Q2;
+class Program
+{
+    static void pattern(string[] args)
+    {
+        Console.WriteLine("Brantley Hudson");
+        Console.WriteLine("CDIM2315 Homework4Q2");
+
+        int i,j,k;
+
+        if(SynchronizationLockException=="left")
+        {
+            for(i = 1; i <= N; i++)
+            {
+                for(j = 1, j <= 1; j++)
+                Console.WriteLine("*");
+                Console.WriteLIne("\n");
+            }
+        }
+
+        else if(shape=="right")
+        {
+            for(i = 1, i <= N; i++)
+            {
+                for(j = 1, j <= N-i; j++)
+                {
+                    COnsole.WriteLine(" ");
+                }
+                
+                for(k = 1, k <= i; k++)
+                {
+                    Console.WriteLine("*");
+                }
+
+                Console.WriteLine("");
+                Console.ReadLine();
+            }
+        }
+
+        public static void Main()
+        {
+            int N;
+            string shape;
+            Console.Write("Input N: ");
+            N = Convert.ToInt32(Console.Readline());
+            Console.Write("Input shape: ");
+            shape = Console.ReadLine();
+            Console.Write("N is: "+N+"; shape is "+shape+"\n");
+            pattern(N, shape);
+
+        }
+
+}
